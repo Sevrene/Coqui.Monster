769cd32e6796d89039bf64b3f7a6f318e11effae
Sevrene <Dowd_05@hotmail.com>
2023-03-10 00:43:10 -0600

    Removed Contentful built components

--- src/components/blockRenders.jsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export function BlockTitle({ title }) {
-  return (
-    <Typography variant="h6" gutterBottom>
-      {title}
-    </Typography>
-  );
-}
