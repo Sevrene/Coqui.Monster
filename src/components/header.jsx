3203646faf46671c6684a3a226a5f01a8cb0307c
Sevrene <Dowd_05@hotmail.com>
2023-03-30 01:51:10 -0500

    Allow header background to fade in/out

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,49 @@
-import { AppBar, Box, Toolbar } from "@mui/material";
+import { AppBar, Box, Toolbar, useScrollTrigger } from "@mui/material";
 import { BuilderComponent, builder } from "@builder.io/react";
 import { useEffect, useState } from "react";
+
+import styled from "@emotion/styled";
+
+const StyledAppBar = styled(({ modelData, shouldApplyStyling, ...other }) => (
+  <AppBar {...other} />
+))`
+  transition: box-shadow 0s;
+  background: none;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: ${({ modelData }) => {
+      if (modelData && modelData.backgroundGradient) {
+        const { direction, colors } = modelData.backgroundGradient ?? {};
+        // If there is only one color, use it as the background color
+        if (colors.length === 1) {
+          return colors[0].color ?? "transparent";
+        }
+        // If there are multiple colors, create a linear gradient using them
+        const gradientDirection = `${direction || 0}deg`;
+        const gradientColors = colors
+          .map(
+            (color) =>
+              `${color.color ? color.color : "rgba(0,0,0,0)"} ${
+                color.colorStop
+              }%`
+          )
+          .join(", ");
+        return `linear-gradient(${gradientDirection}, ${gradientColors})`;
+      } else {
+        return "transparent";
+      }
+    }};
+    opacity: ${({ shouldApplyStyling }) => {
+      return shouldApplyStyling ? 1 : 0;
+    }};
+    transition: opacity 0.5s ease-in-out;
+  }
+`;
 
 /**
   Header Component
@@ -26,6 +69,12 @@ function Header() {
     fetchData();
   }, []);
 
+  const trigger = useScrollTrigger({
+    target: window,
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
   // Map elevation values to corresponding numbers
   const elevationMap = {
     none: 0,
@@ -34,41 +83,22 @@ function Header() {
     high: 24,
   };
 
-  let backgroundStyle = {};
-
-  // Once the model data has been fetched, determine the background styling based on any background gradient provided
-  if (modelData) {
-    const { direction, colors } = modelData.backgroundGradient;
-    // If there is only one color, use it as the background color
-    if (colors.length === 1) {
-      backgroundStyle = { backgroundColor: colors[0].color ?? "transparent" };
-    } else {
-      // If there are multiple colors, create a linear gradient using them
-      const gradientDirection = `${direction || 0}deg`;
-      const gradientColors = colors
-        .map(
-          (color) =>
-            `${color.color ? color.color : "rgba(0,0,0,0)"} ${color.colorStop}%`
-        )
-        .join(", ");
-      backgroundStyle = {
-        background: `linear-gradient(${gradientDirection}, ${gradientColors})`,
-      };
-    }
-  }
+  const shouldApplyStyling =
+    modelData?.hideStylingUntilScrolled === false || trigger;
 
   return (
-    <AppBar
+    <StyledAppBar
       position={modelData?.sticky ? "sticky" : "static"}
-      elevation={elevationMap[modelData?.elevation]}
-      sx={{ ...backgroundStyle }}
+      elevation={shouldApplyStyling ? elevationMap[modelData?.elevation] : 0}
+      modelData={modelData}
+      shouldApplyStyling={shouldApplyStyling}
     >
       <Toolbar variant="dense" disableGutters={true}>
         <Box sx={{ width: "100%" }}>
-          <BuilderComponent model="header" data={modelData} />
+          {modelData && <BuilderComponent model="header" data={modelData} />}
         </Box>
       </Toolbar>
-    </AppBar>
+    </StyledAppBar>
   );
 }
 
