9b12e991d1c0cb03c57c27188ce3d69dfe3b0d95
Sevrene <Dowd_05@hotmail.com>
2024-05-21 21:09:41 -0600

    Remove Sanity (Until Ready)

--- sanity/schemas/abstracts/background/solidColor.js
+++
@@ -1,9 +0,0 @@
-const colorSolid = {
-  name: 'colorSolid',
-  title: 'Background Color',
-  type: 'color',
-  validation: (Rule) => Rule.required(), // Required custom validation as validation only checks for the existence of the field, not it actually being properly set
-  hidden: ({ parent }) => parent?.type !== 'solid',
-};
-
-export default colorSolid;
--- sanity/schemas/abstracts/styles/buttonStyles.js
+++
@@ -1,9 +0,0 @@
-import commonStyles from "../core/commonStyles";
-
-const buttonStyles = {
-  fields: [
-    ...commonStyles
-  ],
-};
-
-export default buttonStyles;
