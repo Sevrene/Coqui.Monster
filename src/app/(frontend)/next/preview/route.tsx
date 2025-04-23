a9b9e25ab41886e437903d43ace0081c3d0df708
Sevrene <SevreneDev@outlook.com>
2025-04-23 17:03:20 -0600

    Cleanup Logic

--- src/app/(frontend)/next/preview/route.tsx
+++ src/app/(frontend)/next/preview/route.tsx
@@ -1,56 +1,55 @@
+import type { PayloadRequest, TypedUser } from 'payload';
+
 import configPromise from '@payload-config';
 import { draftMode } from 'next/headers';
 import { redirect } from 'next/navigation';
-import type { PayloadRequest } from 'payload';
 import { getPayload } from 'payload';
 
-export async function GET(
-  req: {
-    cookies: {
-      get: (name: string) => {
-        value: string;
-      };
-    };
-  } & Request
-): Promise<Response> {
-  const payload = await getPayload({ config: configPromise });
-
+export async function GET(req: Request): Promise<Response> {
+  // Get draft mode early to disable it if any checks fail
+  const draft = await draftMode();
   const { searchParams } = new URL(req.url);
 
-  const preview = searchParams.get('preview');
-  const previewSecret = searchParams.get('previewSecret');
-
+  // Check if the request is a preview request
+  const preview: string | null = searchParams.get('preview');
   if (preview !== 'true') {
-    return new Response(
-      'This endpoint can only be used for relative previews',
-      { status: 500 }
-    );
+    draft.disable();
+    return new Response('This endpoint can only be used for previews', {
+      status: 500,
+    });
   }
 
+  // Check if the preview secret is provided and matches the environment variable
+  const previewSecret: string | null = searchParams.get('previewSecret');
   if (previewSecret !== process.env.PREVIEW_SECRET) {
+    draft.disable();
     return new Response('You are not allowed to preview this page', {
       status: 403,
     });
   }
 
-  let user;
+  // Query the Payload user from the request
+  const payload = await getPayload({ config: configPromise });
+  let user: TypedUser | null = null;
 
   try {
-    user = await payload.auth({
-      req: req as unknown as PayloadRequest,
-      headers: req.headers,
-    });
+    user = await payload
+      .auth({
+        req: req as PayloadRequest,
+        headers: req.headers,
+      })
+      .then((auth) => (auth?.user ? auth.user : null));
   } catch (error) {
     payload.logger.error(
       { err: error },
       'Error verifying token for live preview'
     );
+
+    draft.disable();
     return new Response('You are not allowed to preview this page', {
       status: 403,
     });
   }
-
-  const draft = await draftMode();
 
   if (!user) {
     draft.disable();
@@ -59,9 +58,7 @@ export async function GET(
     });
   }
 
-  // You can add additional checks here to see if the user is allowed to preview this page
-
+  // Enter Preview Mode
   draft.enable();
-
   redirect('/?preview=true');
 }
