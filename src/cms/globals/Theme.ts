c6912c254f1ff1b743c8fc5d778a49ac7d3b8958
Sevrene <SevreneDev@outlook.com>
2025-05-01 04:54:01 -0600

    colorSchemes suck

--- src/cms/globals/Theme.ts
+++ src/cms/globals/Theme.ts
@@ -30,38 +30,6 @@ const Theme: GlobalConfig = {
           relationTo: 'colors',
           required: true,
         },
-        {
-          name: 'darkModeEnabled',
-          type: 'checkbox',
-          label: 'Add Dark Mode',
-        },
-        {
-          name: 'darkMode',
-          admin: {
-            condition: (_, siblingData) => siblingData.darkModeEnabled,
-          },
-          type: 'group',
-          fields: [
-            {
-              name: 'primary',
-              type: 'relationship',
-              relationTo: 'colors',
-              required: true,
-            },
-            {
-              name: 'secondary',
-              type: 'relationship',
-              relationTo: 'colors',
-              required: true,
-            },
-            {
-              name: 'links',
-              type: 'relationship',
-              relationTo: 'colors',
-              required: true,
-            },
-          ],
-        },
       ],
     },
   ],
--- src/providers/themeWrapper.jsx
+++ src/providers/themeWrapper.jsx
@@ -9,46 +9,24 @@ export default function ThemeWrapper({ themeConfig, children }) {
     return <ThemeProvider theme={baseTheme}>{children}</ThemeProvider>;
 
   const theme = createTheme({
-    colorSchemes: {
-      light: {
-        palette: {
-          mode: 'light',
-          primary: {
-            main:
-              themeConfig.colors.primary?.color ||
-              baseTheme.palette.primary.main,
-          },
-          secondary: {
-            main:
-              themeConfig.colors.secondary?.color ||
-              baseTheme.palette.secondary.main,
-          },
-          link: baseTheme.palette.augmentColor({
-            name: 'link',
-            color: {
-              main: themeConfig.colors.links?.color || '#000',
-            },
-          }),
-        },
+    palette: {
+      mode: 'dark',
+      primary: {
+        main:
+          themeConfig.colors.primary?.color || baseTheme.palette.primary.main,
       },
-      ...(themeConfig.darkModeEnabled && {
-        dark: {
-          palette: {
-            mode: 'dark',
-            primary: {
-              main: themeConfig.colors.darkMode.primary?.color,
-            },
-            secondary: {
-              main: themeConfig.colors.darkMode.secondary?.color,
-            },
-            link: baseTheme.palette.augmentColor({
-              name: 'link',
-              color: {
-                main: themeConfig.colors.darkMode.links?.color || '#000',
-              },
-            }),
+      secondary: {
+        main:
+          themeConfig.colors.secondary?.color ||
+          baseTheme.palette.secondary.main,
+      },
+      ...(themeConfig.colors.links?.color && {
+        link: baseTheme.palette.augmentColor({
+          name: 'link',
+          color: {
+            main: themeConfig.colors.links.color,
           },
-        },
+        }),
       }),
     },
   });
